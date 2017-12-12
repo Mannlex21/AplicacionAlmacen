fb744154fa621a3d9c60ba1b08e9136bcd16d170
Mannlex21 <maalmurilloma@ittepic.edu.mx>
2017-12-12 10:52:42 -0600

    MAC

---
+++ AplicacionAlmacen/AplicacionAlmacen/Controlador/Principal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AplicacionAlmacen;
+using AplicacionAlmacen.Modelo;
+
+namespace AplicacionAlmacen.Controlador
+{
+    class Principal
+    {
+        public List<Materiales> metodo()
+        {
+            AlmacenEntities db = new AlmacenEntities();
+            var context = new AlmacenEntities();
+            var connection = context.Database.Connection;
+            Configuracion e = new Configuracion();
+            using (SqlConnection con = new SqlConnection(connection.ConnectionString))
+            {
+                string s;
+                string query = "SELECT*FROM DetallesUsuarios";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    //cmd.Parameters.AddWithValue("@idUsuario", idUsuario);
+                    //s = cmd.ExecuteScalar().ToString();
+                    s = cmd.ToString();
+                    con.Close();
+                    return db.Materiales.ToList();
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ AplicacionAlmacen/AplicacionAlmacen/Vista/Principal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using AplicacionAlmacen.Controlador;
+
+namespace AplicacionAlmacen
+{
+    public partial class Principal : DevExpress.XtraEditors.XtraForm
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Controlador.Principal p = new Controlador.Principal();
+            System.Diagnostics.Debug.WriteLine(p.metodo().FirstOrDefault().marca);
+        }
+    }
+}
